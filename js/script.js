0ff8ad955c524e316182ed00c998cafa335ff740
Wiktor G <wiktor.gebka@gmail.com>
2021-03-23 00:28:46 +0100

    try prompt function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 var randomNumber;
+var playerMove, playerInput;
 var computerMove;
+playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+console.log("wpisana odpowiedź to : " + playerInput)
+if (playerInput == "1"){
+	playerMove = "kamień"
+} else if (playerInput == "2"){
+	playerMove = "papier";
+} else if (playerInput == "3"){
+		playerMove = "nożyce";
+	}
+ else {
+	printMessage("błędny wybór")
+
+}
+printMessage("Twój ruch to: " + playerMove);
+
 randomNumber = Math.floor(Math.random() * 3 + 1);
 printMessage("wylosowana liczba to : " + randomNumber);
 if (randomNumber == "1"){
